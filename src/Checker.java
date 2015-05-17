d1f481cbfaf55de4121bf6acc4c47207fa009026
Pranav Ramarao <pranav.rr93@gmail.com>
2015-05-17 12:49:47 +0530

    modified Checker

--- src/Checker.java
+++ src/Checker.java
@@ -1,11 +1,4 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 public class Checker {
 
 	public static void main(String[] args) throws IOException {
