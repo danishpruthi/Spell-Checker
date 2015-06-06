0e632bf810962e800c77911ef122b26fc538ca6b
Danish <danish037@gmail.com>
2015-06-06 12:56:33 +0530

    Added functionality to ignore undesired words.

--- src/Constants.java
+++ src/Constants.java
@@ -1,7 +1,10 @@
 public class Constants {
-	static String BIGRAM_FILE = "C:/Users/Pranav/git/Spell-Checker/data/bigram.txt";
-	static String UNIGRAM_FILE = "C:/Users/Pranav/git/Spell-Checker/data/unigram.txt";
-	static String MISTAKES_FILE = "C:/Users/Pranav/git/Spell-Checker/data/errors.txt";
-	static String WORDS_FILE = "C:/Users/Pranav/git/Spell-Checker/data/words.txt";
-	static String TEST_FILE = "C:/Users/Pranav/git/Spell-Checker/data/test.txt";
+
+	static String BIGRAM_FILE = "/Users/danish/Documents/Spell Checker/Checker/data/bigram.txt";
+	static String UNIGRAM_FILE = "/Users/danish/Documents/Spell Checker/Checker/data/unigram.txt";
+	static String MISTAKES_FILE = "/Users/danish/Documents/Spell Checker/Checker/data/errors.txt";
+	static String WORDS_FILE = "/Users/danish/Documents/Spell Checker/Checker/data/words.txt";
+	static String TEST_FILE = "/Users/danish/Documents/Spell Checker/Checker/data/sample_test.txt";
+	static String IGNORE_FILE = "/Users/danish/Documents/Spell Checker/Checker/data/ignore.txt";
+
 }
